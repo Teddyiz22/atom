69ec9d24755c83e738328de953ef6f1c5f1ef85a
Bhone Pyae Aung <bhone.pa@7thcomputing.com>
2026-03-04 23:03:23 +0800

    add blog

--- app.js
+++ app.js
@@ -99,8 +99,8 @@ app.use((req, res, next) => {
 
 // Middleware
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(methodOverride('_method'));
 
